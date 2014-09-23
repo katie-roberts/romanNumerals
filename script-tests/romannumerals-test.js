5a5461b9fb8fd8215d89cec4489b21e6c5dd86d2
Katie Stockton Roberts <katie.roberts@bbc.co.uk>
2014-09-23 09:08:18 +0100

    changing the name of the class to test - changed a test name

--- script-tests/romannumerals-test.js
+++ script-tests/romannumerals-test.js
@@ -9,65 +9,65 @@ describe("Roman Numerals Tests", function () {
 
     describe("Sets the bounding limits", function () {
         it("does not accept numbers over 3999", function () {
-            expect(romanNumerals.getNumberAsString(4000)).toBe(romanNumerals.NUMBER_OUT_OF_BOUNDS);
+            expect(romanNumerals.getNumberAsRomanNumeral(4000)).toBe(romanNumerals.NUMBER_OUT_OF_BOUNDS);
         });
 
         it("does not accept numbers below 0", function () {
-            expect(romanNumerals.getNumberAsString(0)).toBe(romanNumerals.NUMBER_OUT_OF_BOUNDS);
+            expect(romanNumerals.getNumberAsRomanNumeral(0)).toBe(romanNumerals.NUMBER_OUT_OF_BOUNDS);
         });
     });
 
 
     describe("returns numbers as expected", function () {
         it("returns 1 as I", function () {
-            expect(romanNumerals.getNumberAsString(1)).toBe("I");
+            expect(romanNumerals.getNumberAsRomanNumeral(1)).toBe("I");
         });
         it("returns 5 as V", function () {
-            expect(romanNumerals.getNumberAsString(5)).toBe("V");
+            expect(romanNumerals.getNumberAsRomanNumeral(5)).toBe("V");
         });
         it("returns 10 as X", function () {
-            expect(romanNumerals.getNumberAsString(10)).toBe("X");
+            expect(romanNumerals.getNumberAsRomanNumeral(10)).toBe("X");
         });
         it("returns 50 as L", function () {
-            expect(romanNumerals.getNumberAsString(50)).toBe("L");
+            expect(romanNumerals.getNumberAsRomanNumeral(50)).toBe("L");
         });
         it("returns 100 as C", function () {
-            expect(romanNumerals.getNumberAsString(100)).toBe("C");
+            expect(romanNumerals.getNumberAsRomanNumeral(100)).toBe("C");
         });
         it("returns 500 as D", function () {
-            expect(romanNumerals.getNumberAsString(500)).toBe("D");
+            expect(romanNumerals.getNumberAsRomanNumeral(500)).toBe("D");
         });
         it("returns 1000 as M", function () {
-            expect(romanNumerals.getNumberAsString(1000)).toBe("M");
+            expect(romanNumerals.getNumberAsRomanNumeral(1000)).toBe("M");
         });
         it("returns 4 as IV", function () {
-            expect(romanNumerals.getNumberAsString(4)).toBe("IV");
+            expect(romanNumerals.getNumberAsRomanNumeral(4)).toBe("IV");
         });
         it("returns 8 as VIII", function () {
-            expect(romanNumerals.getNumberAsString(8)).toBe("VIII");
+            expect(romanNumerals.getNumberAsRomanNumeral(8)).toBe("VIII");
         });
         it("returns 9 as IX", function () {
-            expect(romanNumerals.getNumberAsString(9)).toBe("IX");
+            expect(romanNumerals.getNumberAsRomanNumeral(9)).toBe("IX");
         });
         it("returns 40 as XL", function () {
-            expect(romanNumerals.getNumberAsString(40)).toBe("XL");
+            expect(romanNumerals.getNumberAsRomanNumeral(40)).toBe("XL");
         });
         it("returns 90 as XC", function () {
-            expect(romanNumerals.getNumberAsString(90)).toBe("XC");
+            expect(romanNumerals.getNumberAsRomanNumeral(90)).toBe("XC");
         });
         it("returns 400 as CD", function () {
-            expect(romanNumerals.getNumberAsString(400)).toBe("CD");
+            expect(romanNumerals.getNumberAsRomanNumeral(400)).toBe("CD");
         });
         it("returns 900 as CM", function () {
-            expect(romanNumerals.getNumberAsString(900)).toBe("CM");
+            expect(romanNumerals.getNumberAsRomanNumeral(900)).toBe("CM");
         });
     });
 
     describe("It is able to work out complex numbers", function () {
-        it("is able to convert complex numbers to string 1000's, 100's, 10's", function () {
-            expect(romanNumerals.getNumberAsString(3052)).toBe("MMMLII");
-            expect(romanNumerals.getNumberAsString(1910)).toBe("MCMX");
-            expect(romanNumerals.getNumberAsString(1903)).toBe("MCMIII");
+        it("is able to convert complex numbers to roman numerals", function () {
+            expect(romanNumerals.getNumberAsRomanNumeral(3052)).toBe("MMMLII");
+            expect(romanNumerals.getNumberAsRomanNumeral(1910)).toBe("MCMX");
+            expect(romanNumerals.getNumberAsRomanNumeral(1903)).toBe("MCMIII");
         });
     });
 
