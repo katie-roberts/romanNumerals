1b155a9ab3d3b12671a743b8ac0df2681647c7cf
Katie Roberts <katie.roberts@bbc.co.uk>
2014-12-09 21:47:46 +0000

    expanded the romanNumerals file to include a parser taking in roman numerals and returning integers

--- script-tests/romannumerals-test.js
+++ script-tests/romannumerals-test.js
@@ -1,86 +1,95 @@
-describe("Roman Numerals Tests", function () {
+describe("Roman Numerals Tests", function() {
 
-    var romanNumerals;
+	var romanNumerals;
 
-    beforeEach(function () {
+	beforeEach(function() {
 
-        romanNumerals = new RomanNumerals();
-    });
+		romanNumerals = new RomanNumerals();
+	});
 
-    describe("Sets the bounding limits", function () {
-        it("does not accept numbers over 3999", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(4000)).toBe(romanNumerals.NUMBER_OUT_OF_BOUNDS);
-        });
+	describe("Sets the bounding limits", function() {
+		it("does not accept numbers over 3999", function() {
+			expect(romanNumerals.generate(4000)).toBe(romanNumerals.NUMBER_OUT_OF_BOUNDS);
+		});
 
-        it("does not accept numbers below 0", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(0)).toBe(romanNumerals.NUMBER_OUT_OF_BOUNDS);
-        });
-    });
+		it("does not accept numbers below 0", function() {
+			expect(romanNumerals.generate(0)).toBe(romanNumerals.NUMBER_OUT_OF_BOUNDS);
+		});
+	});
 
 
-    describe("returns numbers as expected", function () {
-        it("returns 1 as I", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(1)).toBe("I");
-        });
-        it("returns 5 as V", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(5)).toBe("V");
-        });
-        it("returns 10 as X", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(10)).toBe("X");
-        });
-        it("returns 50 as L", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(50)).toBe("L");
-        });
-        it("returns 100 as C", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(100)).toBe("C");
-        });
-        it("returns 500 as D", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(500)).toBe("D");
-        });
-        it("returns 1000 as M", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(1000)).toBe("M");
-        });
-        it("returns 4 as IV", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(4)).toBe("IV");
-        });
-        it("returns 8 as VIII", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(8)).toBe("VIII");
-        });
-        it("returns 9 as IX", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(9)).toBe("IX");
-        });
-        it("returns 40 as XL", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(40)).toBe("XL");
-        });
-        it("returns 90 as XC", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(90)).toBe("XC");
-        });
-        it("returns 400 as CD", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(400)).toBe("CD");
-        });
-        it("returns 900 as CM", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(900)).toBe("CM");
-        });
-    });
+	describe("returns numbers as expected", function() {
+		it("returns 1 as I", function() {
+			expect(romanNumerals.generate(1)).toBe("I");
+		});
+		it("returns 5 as V", function() {
+			expect(romanNumerals.generate(5)).toBe("V");
+		});
+		it("returns 10 as X", function() {
+			expect(romanNumerals.generate(10)).toBe("X");
+		});
+		it("returns 50 as L", function() {
+			expect(romanNumerals.generate(50)).toBe("L");
+		});
+		it("returns 100 as C", function() {
+			expect(romanNumerals.generate(100)).toBe("C");
+		});
+		it("returns 500 as D", function() {
+			expect(romanNumerals.generate(500)).toBe("D");
+		});
+		it("returns 1000 as M", function() {
+			expect(romanNumerals.generate(1000)).toBe("M");
+		});
+		it("returns 4 as IV", function() {
+			expect(romanNumerals.generate(4)).toBe("IV");
+		});
+		it("returns 8 as VIII", function() {
+			expect(romanNumerals.generate(8)).toBe("VIII");
+		});
+		it("returns 9 as IX", function() {
+			expect(romanNumerals.generate(9)).toBe("IX");
+		});
+		it("returns 40 as XL", function() {
+			expect(romanNumerals.generate(40)).toBe("XL");
+		});
+		it("returns 90 as XC", function() {
+			expect(romanNumerals.generate(90)).toBe("XC");
+		});
+		it("returns 400 as CD", function() {
+			expect(romanNumerals.generate(400)).toBe("CD");
+		});
+		it("returns 900 as CM", function() {
+			expect(romanNumerals.generate(900)).toBe("CM");
+		});
+	});
 
-    describe("It is able to work out complex numbers", function () {
-        it("is able to convert complex numbers to roman numerals", function () {
-            expect(romanNumerals.getNumberAsRomanNumeral(3052)).toBe("MMMLII");
-            expect(romanNumerals.getNumberAsRomanNumeral(1910)).toBe("MCMX");
-            expect(romanNumerals.getNumberAsRomanNumeral(1903)).toBe("MCMIII");
-        });
-    });
+	describe("It is able to work out complex numbers", function() {
+		it("is able to convert complex numbers to roman numerals", function() {
+			expect(romanNumerals.generate(3052)).toBe("MMMLII");
+			expect(romanNumerals.generate(1910)).toBe("MCMX");
+			expect(romanNumerals.generate(1903)).toBe("MCMIII");
+		});
+	});
 
-    // testing private functions - I have left in to show my working
-    describe("It is able to split large numers up into thousands, hundreds, tens and ones objects", function () {
-        it("is able to split large numbers into 1000's, 100's, 10's", function () {
-            var expectedResponse = [{value: 3000, type: 1000},
-                                    {value: 0, type: 100},
-                                    {value: 50, type: 10},
-                                    {value: 2, type: 1}];
-            expect(romanNumerals._getNumberInRomanNumeralsFormat(3052)).toEqual(expectedResponse);
-        });
-    });
+	describe("It takes a string and if valid converts to integer", function() {
+		it("checks that the given string is a valid Roman Numberal", function() {
+			expect(romanNumerals.parseString("KVII")).toBe(romanNumerals.NOT_VALID_ROMAN_NUMERAL);
+			expect(romanNumerals.parseString("MCKVII")).toBe(romanNumerals.NOT_VALID_ROMAN_NUMERAL);
+		})
 
+		it("is able to convert simple numerals to integers", function() {
+			expect(romanNumerals.parseString("I")).toEqual(1);
+			expect(romanNumerals.parseString("II")).toEqual(2);
+			expect(romanNumerals.parseString("III")).toEqual(3);
+			expect(romanNumerals.parseString("V")).toEqual(5);
+		})
+
+		it("is able to convert more complex numerals to integers", function() {
+			expect(romanNumerals.parseString("IV")).toEqual(4);
+			expect(romanNumerals.parseString("IX")).toEqual(9);
+			expect(romanNumerals.parseString("MCMIII")).toEqual(1903);
+			expect(romanNumerals.parseString("MMMLII")).toEqual(3052);
+		})
+	})
 
 });
--- script/romannumerals.js
+++ script/romannumerals.js
@@ -1,79 +1,145 @@
 /**
  * Roman Numerals Kata
  */
-var RomanNumerals = function () {
+var RomanNumerals = function() {
 
-    this.NUMBER_OUT_OF_BOUNDS = "Number out of bounds";
+	this.NUMBER_OUT_OF_BOUNDS = "Number out of bounds";
+	this.NOT_VALID_ROMAN_NUMERAL = "Not valid Roman Numeral";
 
-    this.romanNumerals = {
-        1: "I",
-        5: "V",
-        10: "X",
-        50: "L",
-        100: "C",
-        500: "D",
-        1000: "M"
-    }
+	this.romanNumerals = {
+		1: "I",
+		5: "V",
+		10: "X",
+		50: "L",
+		100: "C",
+		500: "D",
+		1000: "M"
+	}
 
-    this.getNumberAsRomanNumeral = function (numberToConvert) {
-        if (numberToConvert < 1 || numberToConvert > 3999) {
-            return this.NUMBER_OUT_OF_BOUNDS;
-        }
+	this.generate = function(numberToConvert) { // int to romanNumeral
+		if (numberToConvert < 1 || numberToConvert > 3999) {
+			return this.NUMBER_OUT_OF_BOUNDS;
+		}
 
-        var numeralsArray = this._getNumberInRomanNumeralsFormat(numberToConvert);
-        var numeralsString = "";
+		var numeralsArray = this._getNumberInRomanNumeralsFormat(numberToConvert);
+		var numeralsString = "";
 
-        for (var i = 0; i < numeralsArray.length; i++) {
-            if (numeralsArray[i].value !== 0) {
-                numeralsString += this._convertNumberToString(numeralsArray[i]);
-            }
-        }
-        return numeralsString;
-    }
+		for (var i = 0; i < numeralsArray.length; i++) {
+			if (numeralsArray[i].value !== 0) {
+				numeralsString += this._convertNumberToString(numeralsArray[i]);
+			}
+		}
+		return numeralsString;
+	}
 
+	this.parseString = function(numeralToConvert) {
+		numeralToConvert = numeralToConvert.toUpperCase()
+		if (this._isValidRomanNumeral(numeralToConvert)) {
+			return this._convertNumeralToInt(numeralToConvert);
+		} else {
+			return this.NOT_VALID_ROMAN_NUMERAL;
+		}
+	}
 
-    this._getNumberInRomanNumeralsFormat = function (numberToConvert) {
+	this._isValidRomanNumeral = function(str) {
+		var pattern = '^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$';
+		var res = str.match(pattern);
+		if (res != null) {
+			return true;
+		}
+		return false;
+	}
 
-        var ones = numberToConvert % 10;
-        var tens = ((numberToConvert % 100) - ones);
+	this._convertNumeralToInt = function(str) {
+		var numeralsArray = str.split("");
+		var numbersArray = [];
+		for (var i = 0; i < numeralsArray.length; i++) {
+			switch (numeralsArray[i]) {
+				case "I":
+					numbersArray.push(1)
+					break;
+				case "V":
+					numbersArray.push(5);
+					break;
+				case "X":
+					numbersArray.push(10);
+					break;
+				case "L":
+					numbersArray.push(50);
+					break;
+				case "C":
+					numbersArray.push(100);
+					break;
+				case "D":
+					numbersArray.push(500);
+					break;
+				case "M":
+					numbersArray.push(1000);
+					break;
+			}
+		}
+		return this._workOutValue(numbersArray);
+	}
 
-        var hundreds = ((numberToConvert % 1000) - tens - ones);
-        var thousands = numberToConvert - hundreds - tens - ones;
+	this._workOutValue = function(numbersArray) {
+		var total = 0;
+		for (var i = 0; i < numbersArray.length; i++) {
+			if (i < numbersArray.length - 1) {
+				if (numbersArray[i] < numbersArray[i + 1]) {
+					total = total - numbersArray[i];
+				} else {
+					total = numbersArray[i] + total;
+				}
+			} else {
+				total = numbersArray[i] + total;
+			}
+		}
 
-        var returnArray = [];
-        returnArray.push({value: thousands, type: 1000});
-        returnArray.push({value: hundreds, type: 100});
-        returnArray.push({value: tens, type: 10});
-        returnArray.push({value: ones, type: 1});
+		return total;
+	}
 
-        return returnArray;
-    }
+	this._getNumberInRomanNumeralsFormat = function(numberToConvert) {
 
-    this._convertNumberToString = function (numberToConvert) {
-        for (var num in this.romanNumerals) {
-            if (this.romanNumerals.hasOwnProperty(numberToConvert.type))
-                if (num === numberToConvert.type.toString()) {
-                    var numberOfReturns = numberToConvert.value / numberToConvert.type;
-                    var returnString = "";
-                    if (numberOfReturns >= 5) {
-                        if (numberOfReturns == 9) {
-                            return returnString = this.romanNumerals[numberToConvert.type] + this.romanNumerals[numberToConvert.type * 10];
+		var ones = numberToConvert % 10;
+		var tens = ((numberToConvert % 100) - ones);
 
-                        } else {
-                            returnString = this.romanNumerals[(5 * numberToConvert.type)];
-                            numberOfReturns -= 5;
-                        }
-                    }
-                    if (numberOfReturns === 4) {
-                        return this.romanNumerals[numberToConvert.type] + this.romanNumerals[numberToConvert.type * 5];
-                    }
+		var hundreds = ((numberToConvert % 1000) - tens - ones);
+		var thousands = numberToConvert - hundreds - tens - ones;
 
-                    for (var i = 0; i < numberOfReturns; i++) {
-                        returnString += this.romanNumerals[num];
-                    }
-                    return returnString;
-                }
-        }
-    }
+		var returnArray = [];
+		returnArray.push({value: thousands, type: 1000});
+		returnArray.push({value: hundreds, type: 100});
+		returnArray.push({value: tens, type: 10});
+		returnArray.push({value: ones, type: 1});
+
+		return returnArray;
+	}
+
+	this._convertNumberToString = function(numberToConvert) {
+		for (var num in this.romanNumerals) {
+			if (this.romanNumerals.hasOwnProperty(numberToConvert.type))
+				if (num === numberToConvert.type.toString()) {
+					var numberOfReturns = numberToConvert.value / numberToConvert.type;
+					var returnString = "";
+					if (numberOfReturns >= 5) {
+						if (numberOfReturns == 9) {
+							return returnString = this.romanNumerals[numberToConvert.type] + this.romanNumerals[numberToConvert.type * 10];
+
+						} else {
+							returnString = this.romanNumerals[(5 * numberToConvert.type)];
+							numberOfReturns -= 5;
+						}
+					}
+					if (numberOfReturns === 4) {
+						return this.romanNumerals[numberToConvert.type] + this.romanNumerals[numberToConvert.type * 5];
+					}
+
+					for (var i = 0; i < numberOfReturns; i++) {
+						returnString += this.romanNumerals[num];
+					}
+					return returnString;
+				}
+		}
+	}
 };
 
